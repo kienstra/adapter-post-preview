f80462446880d4d59fbf1bfc932cadedd380da22
Ryan Kienstra <kienstraryan@gmail.com>
2015-01-08 11:03:32 -0300

    changed get_posts to WP_Query

--- includes/class-adapter-post-widget.php
+++ includes/class-adapter-post-widget.php
@@ -2,48 +2,51 @@
 
 class Adapter_Post_Widget extends WP_Widget {
 
-	function __construct() {
+	public function __construct() {
 		$options = array( 'classname' => 'adapter-post-preview' ,
 				  'description' => __( 'Show a carousel of recent posts, or a selected one' , 'adapter-post-preview' ) ,
 		);
 		$this->WP_Widget( 'adapter_post_preview' , __( 'Adapter Post Preview' , 'adapter-post-preview' ) , $options );
 	}
 
-	function form( $instance ) {
+	public function form( $instance ) {
 		$selected_post = isset( $instance[ 'selected_post' ] ) ? $instance[ 'selected_post' ] : "";
 		$selected_post_field_name = $this->get_field_name( 'selected_post' );
 		$selected_post_field_id = $this->get_field_id( 'selected_post' );
-		$posts = get_posts( array( 'type' => 'post' ,
-					   'orderby' => 'date' ,
-					   'posts_per_page' => '100' ,
+		$query = new WP_Query( array( 'post_type'              => 'post' , 
+					      'orderby'	    	       => 'date' ,
+					      'posts_per_page' 	       => '100' ,
+					      'no_found_rows'          => true ,
+					      'update_post_term_cache' => false ,
 		) );
 		?>
 		<p>
-			<label for="<?php echo $selected_post_field_id; ?>">
-				 Post to display:
+			<label for="<?php echo esc_attr( $selected_post_field_id ); ?>">
+				Post to display:
 			</label>
-		<?php
-		if ( $posts ) :
-			?>
-			<select name="<?php echo $selected_post_field_name; ?>" id="<?php echo $selected_post_field_id; ?>" class="widefat appw-post-selector">
+		<?php if ( $query->have_posts() ) : ?>
+			<select name="<?php echo esc_attr( $selected_post_field_name ); ?>" id="<?php echo esc_attr( $selected_post_field_id ); ?>" class="widefat appw-post-selector">
 				<option value="appw_carousel_recent" <?php selected( $selected_post , 'appw_carousel_recent' , true ); ?>>
-					<?php _e( 'Carousel of recent posts' , 'adapter-post-preview' ); ?>
+					<?php esc_html_e( 'Carousel of recent posts' , 'adapter-post-preview' ); ?>
 				</option>
-				<?php foreach( $posts as $post ) : ?>
-					<option value="<?php echo $post->ID ?>" <?php selected( $selected_post , $post->ID , true ); ?>>
-						<?php echo $post->post_title; ?>
-					</option>
-				<?php endforeach; ?>
+				<?php while ( $query->have_posts() ) : 
+					$query->the_post();
+					?>
+						<option value="<?php echo esc_attr( get_the_id() ); ?>" <?php selected( $selected_post , get_the_id() , true ); ?>>
+							<?php echo esc_html( get_the_title() ); ?>
+						</option>
+				<?php endwhile; ?>
 			</select>
-		<?php else :
-			_e( 'No posts on your site. Please write one.' , 'adapter-post-preview' );
-		endif;
+			<?php wp_reset_postdata();
+		else :
+			esc_html_e( 'No posts on your site. Please write one.' , 'adapter-post-preview' );
+		endif; 
 		?>
 		</p>
 		<?php
 	}
 
-	function update( $new_instance , $previous_instance ) {
+	public function update( $new_instance , $previous_instance ) {
 		$instance = $previous_instance;
 		$selected_post = isset( $new_instance[ 'selected_post' ] ) ? $new_instance[ 'selected_post' ] : "";
 		if ( appw_is_valid_value( $selected_post ) ) {
@@ -52,8 +55,7 @@ class Adapter_Post_Widget extends WP_Widget {
 		return $instance;
 	}
 
-	function widget( $args , $instance ) {	// todo : remove extract
-		extract( $args );
+	public function widget( $args , $instance ) {	// todo : remove extract
 		$selected_post	= isset( $instance[ 'selected_post' ] ) ? $instance[ 'selected_post' ] : "";
 		if ( ! $selected_post ) {
 			return;
@@ -64,11 +66,11 @@ class Adapter_Post_Widget extends WP_Widget {
 		else {
 			$markup = $this->get_single_post_preview_without_carousel( $selected_post );
 		}
-
-		echo $before_widget . $markup . $after_widget;
+		
+		echo $args[ 'before_widget' ] . $markup . $args[ 'after_widget' ];
 	}
 
-	private function get_carousel_markup() {
+	protected function get_carousel_markup() {
 		$post_preview_ids = $this->get_post_ids_for_carousel();
 		$post_preview_container = $this->get_all_post_preview_markup( $post_preview_ids );
 
@@ -80,26 +82,26 @@ class Adapter_Post_Widget extends WP_Widget {
 		return $markup;
 	}
 
-	private function get_post_ids_for_carousel() {
+	protected function get_post_ids_for_carousel() {
 		$posts_per_page = apply_filters( 'bwp_number_of_posts_in_carousel' , 5 );
 		global $post;
 		$posts_for_carousel = get_posts( array(
-					'type' => 'post' ,
+					'post_type' => 'post' ,
 					'orderby' => 'date' ,
-					'posts_per_page' => $posts_per_page ,
+					'posts_per_page' => absint( $posts_per_page ) ,
 					'exclude' => isset( $post ) ? $post->ID : "" ,
 		) );
 
 		$post_ids = array();
 		foreach( $posts_for_carousel as $post_for_carousel ) {
 			if ( has_post_thumbnail( $post_for_carousel->ID ) ) {
-			array_push( $post_ids , $post_for_carousel->ID );
+				array_push( $post_ids , $post_for_carousel->ID );
 			}
 		}
 		return $post_ids;
 	}
 
-	private function get_all_post_preview_markup( $post_ids ) {
+	protected function get_all_post_preview_markup( $post_ids ) {
 		global $post;
 		if ( isset( $post ) ) {
 			$post_currently_on_page = $post;
@@ -115,7 +117,7 @@ class Adapter_Post_Widget extends WP_Widget {
 		return $post_preview_container;
 	}
 
-	private function get_markup_for_single_post( $post_id ) {
+	protected function get_markup_for_single_post( $post_id ) {
 		$post = get_post( $post_id );
 		setup_postdata( $post );
 		$post_markup =	appw_get_single_post_preview_markup( $post );
@@ -123,7 +125,7 @@ class Adapter_Post_Widget extends WP_Widget {
 		return $post_markup;
 	}
 
-	private function get_single_post_preview_without_carousel( $post_id ) {
+	protected function get_single_post_preview_without_carousel( $post_id ) {
 		global $post;
 		if ( $post->ID == $post_id ) {
 			return ''; //the post is already showing on the page, so no need for a preview of it
@@ -138,8 +140,8 @@ class Adapter_Post_Widget extends WP_Widget {
 
 
 function appw_get_single_post_preview_markup( $post ) {
-	$thumbnail = get_the_post_thumbnail( $post->ID , 'medium' , array( 'class' => 'img-rounded img-responsive'	) );
-	$title = '<div class="post-title"><h2>' . get_the_title( $post->ID ) . '</h2></div>';
+	$thumbnail = get_the_post_thumbnail( $post->ID , 'medium' , array( 'class' => 'img-rounded img-responsive' ) );
+	$title = '<div class="post-title"><h2>' . esc_html( get_the_title( $post->ID ) ) . '</h2></div>';
 
 	$raw_excerpt = get_the_excerpt();
 	$excerpt_length = apply_filters( 'appw_excerpt_length' , 30 );
@@ -147,7 +149,7 @@ function appw_get_single_post_preview_markup( $post ) {
 
 	$permalink = get_permalink( $post->ID );
 	$link_text = apply_filters( 'appw_link_text' , __( 'Read more' , 'adapter-post-preview' ) );
-	$button = "<a class='btn btn-primary btn-med' href='{$permalink}'>{$link_text}</a>";
+	$button = '<a class="btn btn-primary btn-med" href="' . esc_url( $permalink ) . '">' . esc_html( $link_text ) . '</a>';
 
 	$markup = "<div class='post-preview'>
 			{$thumbnail}
