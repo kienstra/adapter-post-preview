274e5b54337d1da33048f9bd05595eaa641edea3
Ryan Kienstra <kienstraryan@gmail.com>
2018-05-27 23:17:59 -0500

    Remove global constants, in favor of those in Plugin.
    These aren't needed anymore.
    Also, edit the plugin description.

--- adapter-post-preview.php
+++ adapter-post-preview.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Instantiates the Adapter Post Preview plugin
+ * Instantiates the Adapter Post Preview plugin.
  *
  * @package AdapterPostPreview
  */
@@ -10,16 +10,13 @@ namespace AdapterPostPreview;
 /*
 Plugin Name: Adapter Post Preview
 Plugin URI: http://ryankienstra.com/plugins/adapter-post-preview
-Description: Create a widget with a post's featured image, headline, excerpt, and link. If you have Bootstrap 3, make widget with a carousel of recent posts.
+Description: Create a widget with a post's featured image, headline, excerpt, and link. If you have at least Bootstrap 3, you can display a carousel of recent posts.
 
 Version: 1.1
 Author: Ryan Kienstra
 Author URI: www.ryankienstra.com
 License: GPL2
 */
-
-define( 'APPW_PLUGIN_SLUG' , 'adapter-post_preview' );
-define( 'APPW_PLUGIN_VERSION' , '1.0.2' );
 
 require_once dirname( __FILE__ ) . '/php/class-plugin.php';
 $plugin = Plugin::get_instance();
